54d18ace0945c251394ce5ed6cccfadedf3c3c27
kentatixx <kentatixx@gmail.com>
2025-12-14 08:54:25 +0800

    changing the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,6 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Kent Gesoro",
   description: "Minimalist Portfolio",
-  icons: {
-    icon: ["/favicon.ico?v=4"],
-    apple: ["/apple-touch-icon.png"],
-    shortcut: ["/apple-touch-icon.png"],
-  },
 };
 
 export default function RootLayout({
